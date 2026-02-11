0a7c81d6013f4d89d880903160ca55c3fcf4631d
Parthiv panchal <parthiv.panchal.1903@gmail.com>
2026-02-12 01:13:48 +0530

    All done

---
+++ removeduplicat.java
@@ -0,0 +1,28 @@
+//Given a sorted array, remove duplicates in-place. + Return the number of unique elements
+import java.util.*;
+public class removeduplicat{
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)){
+            // ================================ Input ================================
+            System.out.print("Enter the size of the array : ");
+            int size = sc.nextInt();
+            int [] arr = new int[size];
+            for (int i = 0; i < arr.length; i++){
+                System.out.print("Enter the Elemrnt in the array : ");
+                arr[i]=sc.nextInt();
+            }
+            // =============================== Logic To remove Duplicate elements =============================
+            int index = 1;
+            for (int i = 1; i < arr.length; i++){
+                if (arr[i] != arr[i-1]){
+                    arr[index] = arr[i];
+                    index++;
+                }
+            }
+            System.out.println("Unique Elemnet is : "+index );
+            for (int i =0; i<index; i++){
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
