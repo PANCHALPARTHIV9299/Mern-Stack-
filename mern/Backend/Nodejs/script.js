ce5f31126d9152a0a17f14a5e61a46b8331643bc
Parthiv panchal <parthiv.panchal.1903@gmail.com>
2026-02-22 13:33:37 +0530

    done

--- mern/Backend/Nodejs/script.js
+++ mern/Backend/Nodejs/script.js
@@ -1,5 +1,19 @@
-let n = 9;
-for (let i = 0; i < n; i++) {
-    console.log(i);
-    console.log("hello");
-}
+// let n = 9;
+// for (let i = 0; i < n; i++) {
+//     console.log(i);
+//     console.log("hello");
+// }
+
+// let arg = process.argv;
+// for (let i = 2; i < arg.length; i++) {
+//     console.log("Good morning ",arg[i]);
+// }
+
+// const sumValue = require("./Math");
+// console.log(sumValue);
+
+// const Math = require("./Math");
+// console.log(Math.sum(2, 2));
+
+/// ========================gethering info from different foldr / directory==========
+
